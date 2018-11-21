fbbdf16a5b36bd8af313b94824cd37a4ff9442bb
bibodeng <bibodeng@gmail.com>
2018-11-21 17:55:53 +0800

    添加BSV、XSV币种支持

--- pycoin/symbols/xsv.py
+++ pycoin/symbols/xsv.py
@@ -4,7 +4,7 @@ from pycoin.coins.bcash.Tx import Tx as BcashTx
 
 
 network = create_bitcoinish_network(
-    symbol="XCH", network_name="Bcash", subnet_name="testnet3", tx=BcashTx,
+    symbol="XSV", network_name="Bcash", subnet_name="testnet3", tx=BcashTx,
     wif_prefix_hex="ef", sec_prefix="XCHSEC:", address_prefix_hex="6f", pay_to_script_prefix_hex="c4",
     bip32_prv_prefix_hex="04358394", bip32_pub_prefix_hex="043587CF", bech32_hrp="tb",
     magic_header_hex="0B110907", default_port=18333,
